51ad8cbe72ddf6bb4cdfb084e43eebb2fa6f4bd5
MusaSattarov2031 <musasattarov855@gmail.com>
2026-02-19 14:04:17 +0300

    test: pytest fixtures for unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+
+@pytest.fixture()
+def google_xml():
+    with open("data_samples/google_news.xml", "r") as f:
+        return f.read()
+    
+@pytest.fixture
+def empty_xml():
+    with open("data_samples/empty_feed.xml", "r") as f:
+        return f.read()
+    
+@pytest.fixture()
+def error_xml():
+    with open("data_samples/invalid/error_page.xml", "r") as f:
+        return f.read()
+
+@pytest.fixture()   
+def minimal_xml():
+    with open("data_samples/minimal.xml", "r") as f:
+        return f.read()
+    
+@pytest.fixture()
+def nasa_xml():
+    with open("data_samples/nasa_news.xml", "r") as f:
+        return f.read()
+    
+@pytest.fixture()
+def verge_xml():
+    with open("data_samples/the_verge.xml", "r") as f:
+        return f.read()
