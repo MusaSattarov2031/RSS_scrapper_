518a7f9434febddfb8c76b46faa3c0e76bbb4e72
MusaSattarov2031 <musasattarov855@gmail.com>
2026-02-20 14:22:55 +0300

    test: pytest fixtures debug, pytest test for parser

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.fixture()
+@pytest.fixture
 def google_xml():
     with open("data_samples/google_news.xml", "r") as f:
         return f.read()
@@ -10,22 +10,22 @@ def empty_xml():
     with open("data_samples/empty_feed.xml", "r") as f:
         return f.read()
     
-@pytest.fixture()
+@pytest.fixture
 def error_xml():
     with open("data_samples/invalid/error_page.xml", "r") as f:
         return f.read()
 
-@pytest.fixture()   
+@pytest.fixture  
 def minimal_xml():
     with open("data_samples/minimal.xml", "r") as f:
         return f.read()
     
-@pytest.fixture()
+@pytest.fixture
 def nasa_xml():
     with open("data_samples/nasa_news.xml", "r") as f:
         return f.read()
     
-@pytest.fixture()
+@pytest.fixture
 def verge_xml():
     with open("data_samples/the_verge.xml", "r") as f:
         return f.read()
