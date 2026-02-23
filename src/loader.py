99df5c8be4cd66aaf3ac4d9fbc3a646f58e1f16c
MusaSattarov2031 <musasattarov855@gmail.com>
2026-02-23 09:50:12 +0300

    feat: execute_query method for database

--- src/loader.py
+++ src/loader.py
@@ -11,3 +11,10 @@ engine = sa.create_engine(db_url)
 def load_to_sqlite(df: pd.DataFrame, engine: sa.Engine):
     conn = engine.connect()
     df.to_sql("news_articles", conn, if_exists="append", index=False)
+
+def execute_query(query: str, engine: sa.engine):
+    conn = engine.connect()
+
+    result = conn.execute(sa.text(query)).fetchall()
+    conn.commit
+    return result
