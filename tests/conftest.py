6534a475d648abf4f5cda4b83bcb7eb4c1dc55f2
MusaSattarov2031 <musasattarov855@gmail.com>
2026-02-23 09:49:07 +0300

    test: test fixture to get db data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
 import pytest
+from sqlalchemy import text, inspect, create_engine
+import sqlalchemy as sa
+from dotenv import load_dotenv
+from os import getenv
+from src.loader import load_to_sqlite, execute_query
+from src.transformer import transform_to_dataframe
 
 @pytest.fixture
 def google_xml():
@@ -51,3 +57,59 @@ def mock_parsed_data():
             }
         ]
     }
+
+@pytest.fixture(scope="session")
+def db_table_data():
+    load_dotenv()
+    db_url = getenv("DATABASE_URL")
+
+    engine = create_engine(db_url)
+
+    data = {
+        "title": "Engineering Daily",
+        "link": "https://eng-daily.test",
+        "items": [
+            {
+                "title": "First Article",
+                "link": "https://eng-daily.test/article-1",
+                "description": "Clean description.",
+                "pubDate": "Wed, 18 Feb 2026 12:00:00 GMT"
+            },
+            {
+                "title": "First Article (Updated Title)",
+                "link": "https://eng-daily.test/article-1",
+                "description": "Same link, so this is a duplicate!",
+                "pubDate": "Wed, 18 Feb 2026 13:00:00 GMT"
+            },
+            {
+                "title": "Second Article",
+                "link": "https://eng-daily.test/article-2",
+                "description": "Another description.",
+                "pubDate": "Thu, 19 Feb 2026 09:30:00 GMT"
+            }
+        ]
+    }
+
+    df = transform_to_dataframe(data)
+
+    load_to_sqlite(df, engine)
+
+    inspector = inspect(engine)
+    table_name = inspector.get_table_names()[0]
+    rows = execute_query(f"SELECT * FROM {table_name}", engine)
+
+    yield {"table_name": table_name,
+            "columns": [col["name"] for col in inspector.get_columns(table_name)],
+            "rows": rows}
+    
+    print("\n Session Teardown: Cleaning up database after all tests")
+    with engine.connect() as conn:
+        conn.execute(text("Pragma foreign_keys = OFF"))
+        
+        conn.execute(text(f"DELETE FROM {table_name}"))
+
+        conn.execute(text("Pragma foreign_keys = ON"))
+        conn.commit()
+    
+    engine.dispose()
+    print("Session cleanup complete")
